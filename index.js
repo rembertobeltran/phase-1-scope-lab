b8d4eabbed7686e1dae7ff10a4026b55cb69ca9c
Remberto <rembertobeltran@icloud.com>
2022-03-27 19:32:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(string) {
+    return customerName = (customerName.toUpperCase())
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'cindy'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'mono'
+}
